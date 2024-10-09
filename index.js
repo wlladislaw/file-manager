ade9c5f46ed8d525a5d9e0133f04ee5b9f8ca31d
Vladislav <wlladisla@gmail.com>
2024-10-10 00:26:52 +0300

    feat: add greeting

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Welcome to the File Manager, Username!');
+import os from 'os';
+
+const args = process.argv;
+const lastArg = args.find((el) => {
+    if (el.startsWith('--user')) {
+        return el;
+    }
+});
+const userName = lastArg.split('=')[1];
+
+console.log(`Welcome to the File Manager, ${userName}!`);
+
+const homedir = os.homedir();
+
+console.log(`You are currently in ${homedir}`);
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (input) => {
+    if (input.trim() === '.exit') {
+        console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+        process.exit(1);
+    }
+    console.log(`You are currently in ${homedir}`);
+});
+
+process.on('SIGINT', function () {
+    console.log(`\nThank you for using File Manager, ${userName}, goodbye!`);
+    process.exit();
+});
